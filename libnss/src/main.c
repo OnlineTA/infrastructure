60bf77747e16e75e5f618ce40c2d25c532df3917
Oleksandr Shturmov <oleks@oleks.info>
2015-05-16 19:43:00 +0200

    oh-oh

--- libnss/src/main.c
+++ libnss/src/main.c
@@ -1,4 +1,31 @@
-int main()
+#include <nss.h> // nss_status
+#include <pwd.h> // struct passwd
+#include <stdlib.h> // strtoul
+#include <errno.h> // errno
+
+#include <stdio.h>
+
+enum nss_status
+_nss_onlineta_setpwent (void)
 {
-  return 0;
+  return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status
+_nss_onlineta_endpwent (void)
+{
+  return NSS_STATUS_SUCCESS;
+}
+
+enum nss_status
+_nss_onlineta_getpwnam_r(const char *name, struct passwd *result,
+  char *buffer, size_t buflen, int *errnop)
+{
+  printf("HEJ!\n");
+  result->pw_uid = strtoul(name, NULL, 36);
+
+  buffer = buffer;
+  buflen = buflen;
+  *errnop = errno;
+  return NSS_STATUS_NOTFOUND;
 }
